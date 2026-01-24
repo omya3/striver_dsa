ba80b3c6df2d3e4b21333daed00847157bdd2fa9
Omkar Chavan <omkarchavan228@gmail.com>
2026-01-24 20:35:00 +0530

    did find start of loop

---
+++ step_6_linked_list/medium_problem_ll/3_detect_loop_in_ll.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+};
+
+Node *convert_array_to_ll(vector<int> &arr)
+{
+
+    Node *new_node = new Node(arr[0], nullptr);
+    Node *head = new_node;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[0], nullptr);
+        new_node->next = temp;
+        new_node = temp;
+    }
+    return head;
+}
+
+bool detect_loop_in_ll(Node *head)
+{
+
+    if (head == NULL || head->next == nullptr)
+    {
+        return false;
+    }
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+        if (fast == slow)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {2, 3, 8, 7};
+    Node *head = convert_array_to_ll(arr);
+}
---
+++ step_6_linked_list/medium_problem_ll/4_find_starting_point_of_loop.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+};
+
+Node *detect_loop_in_ll(Node *head)
+{
+    Node *slow = head;
+    Node *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+        {
+            slow = head;
+            while (fast != slow)
+            {
+                fast = fast->next;
+                slow = slow->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
