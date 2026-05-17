8679a93f5f20396a9b2698ee88f4dde4958e6cf8
Omkar Chavan <omkarchavan228@gmail.com>
2026-05-17 15:19:11 +0530

    did dp max profit with multiple times buy and sell

---
+++ step_16_DP/step_6_DP_on_stocks/2_max_profit_multiple_buy_sell/1_recursion.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+
+private:
+    int find_max_profit(int ind, int buy, vector<int> &prices, int n)
+    {
+        if (ind == n)
+        {
+            return 0;
+        }
+
+        int profit = 0;
+
+        if (buy)
+        {
+            profit = max(-prices[ind] + find_max_profit(ind + 1, 0, prices, n),
+                         0 + find_max_profit(ind + 1, 1, prices, n));
+        }
+        else
+        {
+            profit = max(prices[ind] + find_max_profit(ind + 1, 1, prices, n),
+                         find_max_profit(ind + 1, 0, prices, n));
+        }
+        return profit;
+    }
+
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        return find_max_profit(0, 1, prices, prices.size());
+    }
+};
