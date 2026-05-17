5224222f4f5ee36958583d8cb9eed4825eb97a92
Omkar Chavan <omkarchavan228@gmail.com>
2026-05-17 11:30:48 +0530

    did min ops to convert str1 to str2

---
+++ step_16_DP/step_5_DP_strings/6_min_ops_to_convert_str1_to_str2/space_optimisation.cpp
@@ -0,0 +1,43 @@
+class Solution
+{
+public:
+    int lcs(string &s1, string &s2)
+    {
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        // Create DP table initialized to 0
+        // Size is (l1 + 1) x (l2 + 1) to handle the base case of empty strings
+        vector<int> prev(l2 + 1, 0);
+
+        // Fill the DP table
+        for (int i = 1; i <= l1; i++)
+        {
+            vector<int> curr(l2 + 1, 0);
+            for (int j = 1; j <= l2; j++)
+            {
+                // If characters match, add 1 to the previous diagonal state
+                if (s1[i - 1] == s2[j - 1])
+                {
+                    curr[j] = 1 + prev[j - 1];
+                }
+                // If they don't match, take the maximum of skipping a character from s1 or s2
+                else
+                {
+                    curr[j] = max(prev[j], curr[j - 1]);
+                }
+            }
+            prev = curr;
+        }
+
+        // The bottom-right cell contains the length of the LCS
+        return prev[l2];
+    }
+
+public:
+    int minDistance(string word1, string word2)
+    {
+        int lcs_len = lcs(word1, word2);
+        return word1.size() + word2.size() - 2 * lcs(word1, word2);
+    }
+};
