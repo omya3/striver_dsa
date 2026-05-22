4b47478caa7119981d54b5cf0f5a6d23b3e023f5
Omkar Chavan <omkarchavan228@gmail.com>
2026-05-22 19:43:32 +0530

    modidfied print LIS

--- step_16_DP/step_7_DP_on_LIS/2_Print_LIS/code.cpp
+++ step_16_DP/step_7_DP_on_LIS/2_Print_LIS/code.cpp
@@ -3,23 +3,29 @@ class Solution
 public:
     vector<int> getLIS(vector<int> &arr)
     {
-        // Code here
         int n = arr.size();
+        if (n == 0)
+            return {};
+
+        // dp[i] stores the length of LIS ending at index i
         vector<int> dp(n, 1);
+        // hash[i] stores the previous element's index in the LIS
+        vector<int> hash(n);
+
         int maxi = 1;
         int lastIndex = 0;
 
         for (int i = 0; i < n; i++)
         {
-            hash[i] = 1;
+            hash[i] = i; // Initialize parent pointer to itself
             for (int prev = 0; prev < i; prev++)
             {
-                if (arr[prev] < arr[i])
+                if (arr[prev] < arr[i] && 1 + dp[prev] > dp[i])
                 {
-                    dp[i] = max(dp[i], 1 + dp[prev]);
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev; // Track the backtracking path
                 }
             }
-
             if (dp[i] > maxi)
             {
                 maxi = dp[i];
@@ -27,19 +33,17 @@ public:
             }
         }
 
+        // Reconstruct the subsequence
         vector<int> temp;
         temp.push_back(arr[lastIndex]);
+
         while (hash[lastIndex] != lastIndex)
         {
             lastIndex = hash[lastIndex];
             temp.push_back(arr[lastIndex]);
         }
-        reverse(temp.begin(), temp.end());
 
-        for (auto it : temp)
-            cout << it << " ";
-            
-        cout << endl;
-        return maxi;
+        reverse(temp.begin(), temp.end());
+        return temp;
     }
 };
