5699ac202cb6d5749ddb6f7b3d6a5ea8618ba05f
Omkar Chavan <omkarchavan228@gmail.com>
2026-06-10 12:19:17 +0530

    modified bfs

--- step_15_graphs/step_1_learning/3_bfs_traversal.cpp
+++ step_15_graphs/step_1_learning/3_bfs_traversal.cpp
@@ -1,32 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+
 using namespace std;
 
-vector<int> bfs(int v, vector<int> adj[])
-{
-    vector<int> visited(v, 0);
-    visited[0] = 1;
-    queue<int> q;
-    q.push(0);
-    vector<int> bfs;
-
-    while (!q.empty())
-    {   
-        int node = q.front();
-        q.pop();
-        bfs.push_back(i);
-        for (auto i : adj[node])
-        {
-            q.push(i);
-            visited[i] = 1;
+class Solution {
+public:
+    vector<int> bfs(vector<vector<int>> &adj) {
+        int v = adj.size(); // Determine the number of vertices
+        vector<int> visited(v, 0);
+        vector<int> bfs_result;
+        queue<int> q;
+        
+        // Start BFS from the first node (assuming node 0)
+        visited[0] = 1;
+        q.push(0);
+        
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            bfs_result.push_back(node); // Fixed: changed 'i' to 'node'
+            
+            for (auto it : adj[node]) { // Iterating through neighbors
+                if (!visited[it]) {     // Fixed: changed 'x' to 'it'
+                    q.push(it);         // Fixed: changed 'i' to 'it'
+                    visited[it] = 1;    // Fixed: changed 'i' to 'it'
+                }
+            }
         }
+        return bfs_result;
     }
-    return bfs;
-}
+};
 
-int main()
-{
-    int v;
-    cin >> v;
-    vector<int> arr[v];
-    bfs(v, arr);
+int main() {
+    int v, e;
+    if (!(cin >> v >> e)) return 0;
+    
+    vector<vector<int>> adj(v);
+    for (int i = 0; i < e; i++) {
+        int u, w;
+        cin >> u >> w;
+        adj[u].push_back(w);
+        adj[w].push_back(u); // Remove this line if the graph is directed
+    }
+    
+    Solution obj;
+    vector<int> ans = obj.bfs(adj);
+    
+    for (int i : ans) {
+        cout << i << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
