481f8a19afc4288060df47b947c73cdb92d6b100
Omkar Chavan <omkarchavan228@gmail.com>
2026-06-24 09:36:58 +0530

    did disjoint set

---
+++ step_15_graphs/step_5_MST/3_disjoint_set.cpp
@@ -0,0 +1,106 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class DisjointSet
+{
+    vector<int> rank, parent, size;
+
+public:
+    // Constructor to initialize the data structure for 'n' elements
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1, 1);
+        for (int i = 0; i <= n; i++)
+        {
+            parent[i] = i;
+        }
+    }
+
+    // Find ultimate parent with path compression optimization
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    // Union operation based on the Rank of trees
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    // Union operation based on the Size of components
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+int main()
+{
+    // Example test case mimicking the code block
+    DisjointSet ds(7);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
+
+    // Check if 3 and 7 belong to the same component
+    if (ds.findUPar(3) == ds.findUPar(7))
+    {
+        cout << "Same\n";
+    }
+    else
+    {
+        cout << "Not same\n";
+    }
+
+    ds.unionBySize(3, 7);
+
+    if (ds.findUPar(3) == ds.findUPar(7))
+    {
+        cout << "Same\n";
+    }
+    else
+    {
+        cout << "Not same\n";
+    }
+    return 0;
+}
